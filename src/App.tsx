ffcb6157b0168459e6bd92253a0122bd47c1e5af
Nitiema Allassane <allassanenitiema@gmail.com>
2026-04-03 00:37:17 +0000

    Creation du composant header de l'App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import './App.css';
 
 
+function Header() {
+  
+  return (
+    <header>
+      <div className='text-center'>
+        <h1 
+          className='text-4xl font-bold bg-linear-to-r from-indigo-500 to-purple-700 
+          bg-clip-text text-transparent mb-1'
+        >
+          Ma liste de tâches
+        </h1>
+        <p className='text-gray-500 text-lg mb-1'>
+          Organisez-vous et restez productif
+        </p>
+        <p className='text-gray-500 text-lg'>
+          <span>1 tâche terminée <span className='font-extrabold'>.</span> 2 tâches au total </span>
+        </p>
+      </div>
+    </header>
+  )
+}
+
+
 function App() {
   
 
   return (
-    <main>
-      
+    <main className='bg-blue-50 min-h-dvh py-2'>
+      <div className="container">
+        <Header  />
+      </div>
     </main>
   )
 }
